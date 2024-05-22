69e039e8a762b5c8a465da1720e2e8b38ce37728
Roby1kenoby <r0by1kenoby@hotmail.it>
2024-05-22 23:25:09 +0200

    Added 1 to 7 extra exercise solutions

--- script.js
+++ script.js
@@ -143,3 +143,155 @@ console.log(acronymize('Amore mio'))
 console.log(acronymize('Pensavo a teeeee'))
 console.log(acronymize('vat\'la pié n\'tla giaca!'))
 console.log('\n')
+
+
+// 1.e) input string output carattere più usato
+
+let findMostUsedChar = function(s1){
+    let chars = {}
+    let topLetter = ''
+    let topLetterCount = 0
+    for (let s of s1){
+        if (chars.hasOwnProperty(s)){
+            chars[s] += 1
+            if (chars[s] > topLetterCount){
+                topLetterCount = chars[s]
+                topLetter = s
+            }
+        }
+        else{
+            chars[s] = 1
+            if(topLetterCount === 0 && topLetter === ''){
+                topLetterCount = 1
+                topLetter = s
+            }
+        }
+    }
+    return topLetter
+}
+
+// test 1.e
+console.log('Test 1.e')
+console.log(findMostUsedChar('sassari'))
+console.log(findMostUsedChar('Bologna'))
+console.log(findMostUsedChar('ciao'))
+console.log(findMostUsedChar('mamma'))
+console.log(findMostUsedChar('Bleeeeeep'))
+console.log(findMostUsedChar('allungo'))
+console.log('\n')
+
+
+// 2.e) input 2 stringhe output true se una è l'anagramma dell'altra
+
+let isAnagram = function(s1, s2){
+    return s1.toLowerCase().split("").sort().join("") === s2.toLowerCase().split("").sort().join("") ? true : false
+}
+
+// test 2.e
+console.log('Test 2.e')
+console.log(isAnagram('sassari', 'irassas'))
+console.log(isAnagram('sassari', 'ssSirAa'))
+console.log(isAnagram('Bologna', 'cipolla'))
+console.log(isAnagram('Bologna', 'bologna'))
+console.log('\n')
+
+// 3.e) input 1 stringa 1 array output stringhe dell'array che sono anagramma della stringa input
+
+let findAnagrams = function(s1, a1){
+    let word = s1.toLowerCase().split("").sort().join("") 
+    let anagrams = []
+    for (let s of a1){
+        s.toLowerCase().split("").sort().join("") === word ? anagrams.push(s) : null
+    }
+
+    return anagrams
+}
+
+// test 3.e
+console.log('Test 3.e')
+console.log(findAnagrams('sassari', ['irassas', 'ssSirAa', 'cipolla']))
+console.log(findAnagrams('cartine', ['carenti', 'incerta', 'espatrio']))
+console.log('\n')
+
+
+// 4.e) input 1 stringa output true se stringa palindroma, altrimenti false
+
+let isPalindrome = function(s1){
+    // reverse della stringa per "leggerla al contrario". Se è uguale allora è palindroma
+    return s1.toLowerCase().split('').join() === s1.toLowerCase().split('').reverse().join() ? true : false
+}
+
+// test 4.e
+console.log('Test 4.e')
+console.log(isPalindrome('mammam'))
+console.log(isPalindrome('ciao sono io oi onos oaic'))
+console.log(isPalindrome('boh che ne so?'))
+console.log('\n')
+
+
+// 5.e) input un numero intero output numero letto dal fondo
+
+let reverseInt = function(i1){
+    let revInt = 0
+    while(i1 > 0){
+        // il modulo mi permette di prendere il resto, che dividendo per 10 è l'ultima cifra del numero.
+        // poi divido per 10 il numero stesso, e uso floor per eliminare la parte dopo la virgola (che tanto non mi serve)
+        // a questo punto, al nuovo giro, prendo cosa c'è nella variabile rev (inizializzata a 0) e moltiplico per 10
+        // per creare al primo giro le decine, poi le centinaia, poi le migliaia e così via
+        revInt = (revInt * 10) + (i1%10) 
+        i1 = Math.floor(i1/10)
+    }
+    return revInt
+}
+
+// test 5.e
+console.log('Test 5.e')
+console.log(reverseInt(981))
+console.log(reverseInt(12347))
+console.log(reverseInt(456))
+console.log('\n')
+
+// 6.e) input un numero intero output scala di #
+
+let scala = function(i1){
+    for (let i = 0; i <i1; i++ ){
+        for (let j = 0; j <= i; j++){
+            console.log("#")
+        }
+        console.log("\n")
+    }
+}
+
+// test 6.e
+console.log('Test 6.e')
+console.log(scala(2))
+console.log(scala(3))
+console.log(scala(4))
+console.log('\n')
+
+
+// 7.e) input string output stringa reversed
+
+let stringReverse = function(s1){
+    return s1.split('').reverse().join('')
+}
+
+// test 7.e
+console.log('Test 7.e')
+console.log(stringReverse('ciao'))
+console.log(stringReverse('pippo'))
+console.log(stringReverse('otrebla'))
+console.log('\n')
+
+// 8.e) input array + intero output array diviso in n parti
+
+let arraySplit = function(a1, n1){
+    
+}
+
+// test 8.e
+console.log('Test 8.e')
+console.log(stringReverse('ciao'))
+console.log(stringReverse('pippo'))
+console.log(stringReverse('otrebla'))
+console.log('\n')
